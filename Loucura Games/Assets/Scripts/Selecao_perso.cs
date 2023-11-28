6ad0548b06702074975426e1d701077718123d14
guileisme <guileisme1@gmail.com>
2023-11-28 10:49:14 -0300

    main base

---
+++ Loucura Games/Assets/Scripts/Selecao_perso.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class Selecao_perso : MonoBehaviour
+{
+    public Animator axoloteanim, cachorroanim;
+    public Image axoloteImage, cachorroImage;
+    private int personagem;
+    private AudioSource som;
+    private Color cor;
+    // Start is called before the first frame update
+    void Start()
+    {
+        personagem = 1;
+        som = GetComponent<AudioSource>();
+        cor = cachorroImage.color;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            personagem = 1;
+            som.Play();
+        }
+        else if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            personagem = 2;
+            som.Play();
+        }
+        if (personagem == 1)
+        {
+            axoloteImage.color = cor;
+            cachorroImage.color = Color.white;
+            cachorroanim.SetBool("Press", true);
+        }
+        else if (personagem == 2)
+        {
+            cachorroImage.color = cor;
+            axoloteImage.color = Color.white;
+            cachorroanim.SetBool("Press", false);
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }        
+    }
+    void PlaySound()
+    {
+        if(!som.isPlaying)
+        {
+            som.Play();
+        }
+    }
+}
