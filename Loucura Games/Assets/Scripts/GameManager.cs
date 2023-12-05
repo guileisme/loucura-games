64ac912eee6884c5c8bb7125dace7622b1f7ea36
Peanutsu <adrianojunior1227@gmail.com>
2023-12-05 18:14:49 -0300

    esse inferno de git é muito complicado

---
+++ Loucura Games/Assets/Scripts/GameManager.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{   
+    public int CharacterIndex;
+    private GameManager gameManager;
+    // Start is called before the first frame update
+    void Awake()
+    { if(gameManager == null)
+        {
+            gameManager = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+        DontDestroyOnLoad(gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
