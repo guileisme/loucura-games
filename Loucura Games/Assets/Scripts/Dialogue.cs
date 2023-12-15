d1ab3498cb743c543bf3396e5bb26c40db0f0419
NewGodBR <newgodbr.mine@gmail.com>
2023-12-15 18:43:15 -0300

    Godolf adicionado de verdade e Interação Com placa corrigida por outros objetos

--- Loucura Games/Assets/Scripts/Dialogue.cs
+++ Loucura Games/Assets/Scripts/Dialogue.cs
@@ -4,12 +4,13 @@ using UnityEngine;
 
 [RequireComponent(typeof(Collider2D))]
 public class Dialogue : MonoBehaviour
-{   
+{
     public Sprite profile;
     public string[] speechTxt;
     public string actorName;
     private bool left;
-    
+    private bool onRadious;
+
 
     public LayerMask playerLayer;
 
@@ -17,11 +18,13 @@ public class Dialogue : MonoBehaviour
 
     private void OnTriggerExit2D(Collider2D collision)
     {
-       left = true;
+        left = true;
+        onRadious = false;
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         left = false;
+        onRadious = true;
     }
 
     private void Start()
@@ -29,17 +32,24 @@ public class Dialogue : MonoBehaviour
         dc = FindObjectOfType<DialogueControl>();
     }
 
+    void Update()
+    {
+        if (Input.GetButtonDown("Interaction") && onRadious)
+        {
+            Interact();
+        }
+    }
     void FixedUpdate()
     {
         if (left)
-        {   
+        {
             dc.End();
             left = false;
         }
     }
-    
+
     public void Interact()
-    {   
-        dc.Speech(profile,speechTxt,actorName);
+    {
+        dc.Speech(profile, speechTxt, actorName);
     }
 }
